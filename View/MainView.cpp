73a35def91c0dcfacd6c7dfd450a4ab7f6500460
Paul Nykiel <paul.nykiel@gmail.com>
2018-12-31 13:33:03 +0100

    Added event handlers, fixed adjustments

--- View/MainView.cpp
+++ View/MainView.cpp
@@ -15,13 +15,17 @@ namespace view {
         Glib::RefPtr<Gtk::Builder> builder = Gtk::Builder::create_from_file(uiFile);
 
         builder->get_widget(WIDGET_MAP(mainWindow));
+        builder->get_widget(WIDGET_MAP(connectButton));
         builder->get_widget(WIDGET_MAP(portCombo));
         builder->get_widget(WIDGET_MAP(baudSpin));
-        builder->get_widget(WIDGET_MAP(connectButton));
         builder->get_widget(WIDGET_MAP(dataBitsSpin));
         builder->get_widget(WIDGET_MAP(stopBitsSpin));
 
         this->connectButton->signal_clicked().connect(sigc::mem_fun(this, &MainView::connectButtonHandler));
+        this->portCombo->signal_changed().connect(sigc::mem_fun(this, &MainView::portComboHandler));
+        this->baudSpin->signal_changed().connect(sigc::mem_fun(this, &MainView::baudSpinHandler));
+        this->dataBitsSpin->signal_changed().connect(sigc::mem_fun(this, &MainView::dataBitsSpinHandler));
+        this->stopBitsSpin->signal_changed().connect(sigc::mem_fun(this, &MainView::stopBitsSpinHandler));
     }
 
     void MainView::setPorts(const std::vector<std::string> &ports, int activeIndex) {
@@ -42,11 +46,67 @@ namespace view {
         }
     }
 
+    void MainView::portComboHandler() {
+        if (this->portComboListener.has_value()) {
+            this->portComboListener.value()(this->portCombo->get_active_text());
+        }
+    }
+
+    void MainView::baudSpinHandler() {
+        if (this->baudSpinListener.has_value()) {
+            this->baudSpinListener.value()(this->baudSpin->get_value_as_int());
+        }
+    }
+
+    void MainView::dataBitsSpinHandler() {
+        if (this->dataBitsSpinListener.has_value()) {
+            this->dataBitsSpinListener.value()(this->dataBitsSpin->get_value_as_int());
+        }
+    }
+
+    void MainView::stopBitsSpinHandler() {
+        if (this->stopBitsSpinListener.has_value()) {
+            this->stopBitsSpinListener.value()(this->stopBitsSpin->get_value_as_int());
+        }
+    }
+
     void MainView::registerConnectButtonListener(std::function<void()> listener) {
         if (this->connectButtonListener.has_value()) {
-            throw std::logic_error("Only on listener possible");
+            throw std::logic_error("Only one listener possible");
         }
 
         this->connectButtonListener = listener;
+    }
+
+    void MainView::registerPortComboListener(std::function<void(std::string)> listener) {
+        if (this->portComboListener.has_value()) {
+            throw std::logic_error("Only one listener possible");
+        }
+
+        this->portComboListener = listener;
+    }
+
+    void MainView::registerBaudSpinListener(std::function<void(int)> listener) {
+        if (this->baudSpinListener.has_value()) {
+            throw std::logic_error("Only one listener possible");
+        }
+
+        this->baudSpinListener = listener;
+    }
+
+    void MainView::registerDataBitsSpinListener(std::function<void(int)> listener) {
+        if (this->dataBitsSpinListener.has_value()) {
+            throw std::logic_error("Only one listener possible");
+        }
+
+        this->dataBitsSpinListener = listener;
+    }
+
+    void MainView::registerStopBitsSpinListener(std::function<void(int)> listener) {
+        if (this->stopBitsSpinListener.has_value()) {
+            throw std::logic_error("Only one listener possible");
+        }
+
+        this->stopBitsSpinListener = listener;
     }
 }
--- View/MainView.hpp
+++ View/MainView.hpp
@@ -21,18 +21,30 @@ namespace view {
         auto getWindow() -> Gtk::Window&;
 
         void registerConnectButtonListener(std::function<void()> listener);
+        void registerPortComboListener(std::function<void(std::string)> listener);
+        void registerBaudSpinListener(std::function<void(int)> listener);
+        void registerDataBitsSpinListener(std::function<void(int)> listener);
+        void registerStopBitsSpinListener(std::function<void(int)> listener);
     private:
         Gtk::Window *mainWindow;
 
         // Top bar
+        Gtk::Button *connectButton;
         Gtk::ComboBoxText *portCombo;
         Gtk::SpinButton *baudSpin;
-        Gtk::Button *connectButton;
         Gtk::SpinButton *dataBitsSpin;
         Gtk::SpinButton *stopBitsSpin;
 
         void connectButtonHandler();
         std::optional<std::function<void()>> connectButtonListener;
+        void portComboHandler();
+        std::optional<std::function<void(std::string)>> portComboListener;
+        void baudSpinHandler();
+        std::optional<std::function<void(int)>> baudSpinListener;
+        void dataBitsSpinHandler();
+        std::optional<std::function<void(int)>> dataBitsSpinListener;
+        void stopBitsSpinHandler();
+        std::optional<std::function<void(int)>> stopBitsSpinListener;
     };
 }
 
