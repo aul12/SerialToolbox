5d2e6294ec1ff8a965cf29a369c6691092353910
Paul Nykiel <paul.nykiel@gmail.com>
2018-12-31 13:31:31 +0100

    Fixed setBaud

--- Util/Serial/InterfacePosix.cpp
+++ Util/Serial/InterfacePosix.cpp
@@ -94,6 +94,103 @@ namespace util::serial {
     }
 
     void InterfacePosix::setBaud(int baud) {
+        speed_t baudBits;
+        switch (baud) {
+            case 50:
+                baudBits = B50;
+                break;
+            case 75:
+                baudBits = B75;
+                break;
+            case 110:
+                baudBits = B110;
+                break;
+            case 134:
+                baudBits = B134;
+                break;
+            case 150:
+                baudBits = B150;
+                break;
+            case 200:
+                baudBits = B200;
+                break;
+            case 300:
+                baudBits = B300;
+                break;
+            case 600:
+                baudBits = B600;
+                break;
+            case 1200:
+                baudBits = B1200;
+                break;
+            case 1800:
+                baudBits = B1800;
+                break;
+            case 2400:
+                baudBits = B2400;
+                break;
+            case 4800:
+                baudBits = B4800;
+                break;
+            case 9600:
+                baudBits = B9600;
+                break;
+            case 19200:
+                baudBits = B19200;
+                break;
+            case 38400:
+                baudBits = B38400;
+                break;
+#ifdef B57600 // Not a part of POSIX.1, but supported by Linux 4.15
+            case 57600:
+                baudBits = B57600;
+                break;
+            case 115200:
+                baudBits = B115200;
+                break;
+            case 230400:
+                baudBits = B230400;
+                break;
+            case 460800:
+                baudBits = B460800;
+                break;
+            case 500000:
+                baudBits = B500000;
+                break;
+            case 576000:
+                baudBits = B576000;
+                break;
+            case 921600:
+                baudBits = B921600;
+                break;
+            case 1000000:
+                baudBits = B1000000;
+                break;
+            case 1152000:
+                baudBits = B1152000;
+                break;
+            case 1500000:
+                baudBits = B1500000;
+                break;
+            case 2000000:
+                baudBits = B2000000;
+                break;
+            case 2500000:
+                baudBits = B2500000;
+                break;
+            case 3000000:
+                baudBits = B3000000;
+                break;
+            case 3500000:
+                baudBits = B3500000;
+                break;
+            case 4000000:
+                baudBits = B4000000;
+                break;
+#endif
+            default:
+                throw std::runtime_error("Not a valid baud");
+        }
         termios tty{};
         memset(&tty, 0, sizeof tty);
         if (tcgetattr(fd, &tty) != 0) {
