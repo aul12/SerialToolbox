b81ec7b75f22e5192f78879f336b2060925a3839
Paul Nykiel <paul.nykiel@gmail.com>
2018-12-31 13:32:35 +0100

    Small changes, cleanup

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include "Util/Serial/InterfacePosix.hpp"
 #include "View/MainView.hpp"
 
-using namespace Gtk;
 
 int main(int argc, char *argv[]) {
     auto app = Gtk::Application::create(argc, argv, "me.aul12.term");
