cfdff12a1d8aba01b09cd7e7848085d73c0c668b
Paul Nykiel <paul.nykiel@gmail.com>
2019-01-01 15:24:20 +0100

    More Listener Tests

--- Tests/Util/Listener.cpp
+++ Tests/Util/Listener.cpp
@@ -1,8 +1,8 @@
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 
 #include "Util/Listener.hpp"
 
-TEST(Listener, Add) {
+TEST(Listener, AddGetSubscribed) {
     util::Listener<> listener{};
 
     util::Listener<>::type f = [](){};
@@ -12,4 +12,32 @@ TEST(Listener, Add) {
     EXPECT_EQ(listener.getSubscribed(), 1);
     EXPECT_NO_THROW(listener(f));
     EXPECT_EQ(listener.getSubscribed(), 2);
+}
+
+TEST(Listener, AddCall) {
+    util::Listener<> listener{};
+    volatile int i = 0;
+
+    util::Listener<>::type f = [&i](){i++;};
+
+    EXPECT_NO_THROW(listener(f));
+    EXPECT_NO_THROW(listener());
+    EXPECT_EQ(i, 1);
+    EXPECT_NO_THROW(listener(f));
+    EXPECT_NO_THROW(listener());
+    EXPECT_EQ(i, 3);
+}
+
+TEST(Listner, Args) {
+    util::Listener<int> listener{};
+    volatile int i = 0;
+
+    util::Listener<int>::type f = [&i](int a){i+=a;};
+
+    EXPECT_NO_THROW(listener(f));
+    EXPECT_NO_THROW(listener(2));
+    EXPECT_EQ(i, 2);
+    EXPECT_NO_THROW(listener(f));
+    EXPECT_NO_THROW(listener(2));
+    EXPECT_EQ(i, 6);
 }
